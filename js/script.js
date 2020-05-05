7eacbfd7a4932b6c7a33de99a459e293b68de5d6
hrayr <hrayrgrigoryan93@gmail.com>
2020-05-06 00:53:57 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    function catchData(){
+
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+
+            request.addEventListener('load', function () {
+                if (request.readyState === 4 && request.status == 200) {
+
+                    resolve(this.response);
+                } else {
+                    reject();
+                }
+            });
+            request.send();
+
+        });
+    }
+
+    catchData()
+        .then(response =>{
+            let data = JSON.parse(response);
+            inputUsd.value = inputRub.value / data.usd
+        })
+        .catch(() => inputUsd.value = "Что-то пошло не так!");
+
+});
